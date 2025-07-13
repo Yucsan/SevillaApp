3b51764912144b8e08f4256ba065f0963fed1bde
Yucsan <yucsan2023@gmail.com>
2025-07-14 00:00:56 +0200

    Migración a app.config.js, uso de .env para API key y limpieza del proyecto

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,19 +1,50 @@
+import { useRef } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+
 import { getIconForPlace } from '@/constants/iconMap';
 import { places } from '@/constants/mockData';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
 
 export default function MapScreen() {
+  // Limites geográficos de Sevilla
+  const SEVILLA_BOUNDS = {
+    north: 37.4500,  // latitud máxima
+    south: 37.3500,  // latitud mínima
+    east: -5.9200,   // longitud máxima
+    west: -6.0500,   // longitud mínima
+  };
+
+  const mapRef = useRef<MapView | null>(null);
+
+  const isInsideSevilla = (region: Region) => {
+    return (
+      region.latitude >= SEVILLA_BOUNDS.south &&
+      region.latitude <= SEVILLA_BOUNDS.north &&
+      region.longitude >= SEVILLA_BOUNDS.west &&
+      region.longitude <= SEVILLA_BOUNDS.east
+    );
+  };
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={{
           latitude: 37.3886,
           longitude: -5.9823,
           latitudeDelta: 0.05,
           longitudeDelta: 0.05,
+        }}
+        onRegionChangeComplete={(region) => {
+          if (!isInsideSevilla(region)) {
+            mapRef.current?.animateToRegion({
+              latitude: 37.3886,
+              longitude: -5.9823,
+              latitudeDelta: 0.05,
+              longitudeDelta: 0.05,
+            }, 500);
+          }
         }}
       >
         {places.map((place) => (
@@ -22,7 +53,7 @@ export default function MapScreen() {
             coordinate={{ latitude: place.lat, longitude: place.lng }}
             title={place.nombre}
             description={place.descripcion}
-            image={getIconForPlace(place.id)} // ✅ sin Image wrapper
+            image={getIconForPlace(place.id)}
           />
         ))}
       </MapView>
